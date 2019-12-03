cdb21df92af4255be96e43b6dd35dc87ea4f29a4
updatinglb <updatinglb@sina.com>
2019-12-02 16:19:58 -0800

    workable code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { ControlButton } from "./Components/controlButton.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timer: 0,
+            isRunning: false
+        };
+    }
+
+    handleCounter = () => {
+        this.setState({
+            isRunning: !this.state.isRunning
+        });
+
+        if (this.state.isRunning) {
+            clearInterval(this.myInterval);
+        } else {
+            this.myInterval = setInterval(
+                () =>
+                    this.setState({
+                        timer: this.state.timer + 1
+                    }),
+                1000
+            );
+        }
+    };
+
+    handleReset = () => {
+        this.setState({
+            timer: 0,
+            isRunning: false
+        });
+        clearInterval(this.myInterval);
+    };
+
+    render() {
+        return (
+            <div className="app">
+                {/* <h1>hello world</h1> */}
+                <div className="main">{this.state.timer}</div>
+
+                <div className="control-panel">
+                    <ControlButton
+                        name={this.state.isRunning ? "STOP" : "START"}
+                        handleEvent={this.handleCounter}
+                    ></ControlButton>
+                    <ControlButton
+                        name="RESET"
+                        handleEvent={this.handleReset}
+                    ></ControlButton>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
